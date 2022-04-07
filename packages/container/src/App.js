0dd0e4bbb261dcf04250bc07eb2bc5cacff3d631
Darshan <gosai.darshan88@gmail.com>
2022-04-07 17:25:54 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp';
+
+function App() {
+    return (
+        <div>
+            <MarketingApp />
+            <hr />
+            <h1>
+                Hi There!
+            </h1>
+        </div>
+    )
+}
+
+export default App;
